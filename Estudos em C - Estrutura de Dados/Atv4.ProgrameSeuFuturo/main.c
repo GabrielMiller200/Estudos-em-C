24e8c9d62e0baa8c2f4107ee403ca67ac53b5951
Gabriel Miller <72418427+GabrielMiller200@users.noreply.github.com>
2022-03-12 04:16:15 -0300

    Update main.c

--- Estudos em C - Estrutura de Dados/Atv4.ProgrameSeuFuturo/main.c
+++ Estudos em C - Estrutura de Dados/Atv4.ProgrameSeuFuturo/main.c
@@ -1,12 +1,12 @@
 /* Uma pequena loja de artesanato possui apenas um vendedor e comercializa 10 tipos de objetos
-diferentes. O vendedor recebe um sal·rio de R$1100,00 acrescido de 5% do valor total de suas
-vendas. O valor unit·rio dos objetos deve ser informado e armazenado em um vetor; a quantidade
-vendida de cada objeto deve ficar em outro vetor, mas na mesma posiÁ„o. Crie um programa que
-receba os preÁos e as quantidades vendidas, armazenando-os em seus respectivos vetores. Depois,
+diferentes. O vendedor recebe um sal√°rio de R$1100,00 acrescido de 5% do valor total de suas
+vendas. O valor unit√°rio dos objetos deve ser informado e armazenado em um vetor; a quantidade
+vendida de cada objeto deve ficar em outro vetor, mas na mesma posi√ß√£o. Crie um programa que
+receba os pre√ßos e as quantidades vendidas, armazenando-os em seus respectivos vetores. Depois,
 determine e mostre:
-a) A quantidade vendida, valor unit·rio e valor total de cada objeto. Ao final, dever„o ser mostrados
-  o valor total das vendas e o valor da comiss„o que ser· paga ao vendedor.
-b) O valor do objeto mais vendido e sua posiÁ„o no vetor (em caso de empates mostre todos
+a) A quantidade vendida, valor unit√°rio e valor total de cada objeto. Ao final, dever√£o ser mostrados
+  o valor total das vendas e o valor da comiss√£o que ser√° paga ao vendedor.
+b) O valor do objeto mais vendido e sua posi√ß√£o no vetor (em caso de empates mostre todos
 empatados). */
 
 #include <stdio.h>
@@ -54,10 +54,10 @@ int main()
         scanf("%s", nome);
         strcpy(estoque[i].produto, nome);
 
-        printf(" Valor unitario: ",i+1);
+        printf(" Valor unitario: ");
         scanf("%f", &estoque[i].valor);
 
-        printf(" Unidades vendidas: ",i+1);
+        printf(" Unidades vendidas: ");
         scanf("%d", &estoque[i].quantidade);
     }
 

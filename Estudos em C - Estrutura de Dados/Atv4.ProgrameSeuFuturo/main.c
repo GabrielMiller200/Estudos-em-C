8109b71b37ecf00b2dcc46cf709b9317e626a7df
Gabriel Miller <72418427+GabrielMiller200@users.noreply.github.com>
2022-03-12 05:02:15 -0300

    Update main.c

--- Estudos em C - Estrutura de Dados/Atv4.ProgrameSeuFuturo/main.c
+++ Estudos em C - Estrutura de Dados/Atv4.ProgrameSeuFuturo/main.c
@@ -1,8 +1,9 @@
-/* Uma pequena loja de artesanato possui apenas um vendedor e comercializa 10 tipos de objetos
+/* Uma pequena loja de artesanato possui apenas um vendedor e comercializa ?? tipos de objetos
 diferentes. O vendedor recebe um salário de R$1100,00 acrescido de 5% do valor total de suas
-vendas. O valor unitário dos objetos deve ser informado e armazenado em um vetor; a quantidade
-vendida de cada objeto deve ficar em outro vetor, mas na mesma posição. Crie um programa que
-receba os preços e as quantidades vendidas, armazenando-os em seus respectivos vetores. Depois,
+vendas.
+O valor unitário dos objetos deve ser informado e armazenado em um vetor; a quantidade
+vendida de cada objeto deve ficar em outro vetor, mas na mesma posição.
+Crie um programa que receba os preços e as quantidades vendidas, armazenando-os em seus respectivos vetores. Depois,
 determine e mostre:
 a) A quantidade vendida, valor unitário e valor total de cada objeto. Ao final, deverão ser mostrados
   o valor total das vendas e o valor da comissão que será paga ao vendedor.
@@ -15,7 +16,6 @@ empatados). */
 
 #define salario 1100.00
 #define porcent 0.05
-
 
 struct estoque
 {
@@ -30,7 +30,7 @@ int main()
 
     int tam = 0;
     char nome[50];
-    float val, total = 0, pagamento = 0;
+    float total = 0, pagamento = 0;
     int quant = 0, aux;
 
     printf(" \n\t --- LOJA DE ARTESANATO ---\n\n");
@@ -39,26 +39,28 @@ int main()
     printf(" Digite a Quantidade de Produtos diferentes\n");
     printf("\n Produtos unicos: ");
     scanf("%d", &tam);
+    fflush(stdin);
 
     Estoque* estoque = (Estoque *) malloc(sizeof(Estoque ) * tam);
 
     printf("\n ----------------------------------------------\n");
     printf("\n Digite os detalhes sobre o seu Produto\n");
 
-    printf("\n * SE O NOME quantER ESPACOS, DIGITAR JUNTO OU '.'\n EXEMPLO : JarraDeSuco OU Jarra.de.Suco\n");
-
     for(int i=0; i < tam; i++)
     {
         printf("\n -- Produto %d --",i+1);
-        printf("\n Nome: ",i+1);
-        scanf("%s", nome);
+        printf("\n Nome: ");
+        fgets(nome,50,stdin);
+        fflush(stdin);
         strcpy(estoque[i].produto, nome);
 
         printf(" Valor unitario: ");
         scanf("%f", &estoque[i].valor);
+        fflush(stdin);
 
         printf(" Unidades vendidas: ");
         scanf("%d", &estoque[i].quantidade);
+        fflush(stdin);
     }
 
     printf("\n ----------------------------------------------\n");
